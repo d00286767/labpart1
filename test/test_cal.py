285d9a4c6a29d312172a1f92a79a5255145926ca
Jaiwin2000 <jewinmax1000@gmail.com>
2026-05-06 14:46:38 +0100

    Testfile and folder

---
+++ test/test_cal.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+import part1
+import pytest
+
+def test_calculate_inputs():
+    #Test invalid inputs
+    with pytest.raises(ValueError):
+        part1.calculate_interest("jai")  
+    with pytest.raises(ValueError):
+        part1.calculate_interest(True)   
+    with pytest.raises(ValueError):
+        part1.calculate_interest(False)
+    with pytest.raises(ValueError):
+        part1.calculate_interest(-3)
+    with pytest.raises(ValueError):
+        part1.calculate_interest(-3.4)
+
+#test interest calculations for various deposit amounts
+@pytest.mark.parametrize("deposit, expected_interest", [
+    (1000, 30.0),  
+    (10000, 345.0), 
+    (100000, 3940.0), 
+    (150000, 6190.0)
+])
+def test_calculate_interest(deposit, expected_interest):
+    assert part1.calculate_interest(deposit) == f"{expected_interest:.2f}"
+
